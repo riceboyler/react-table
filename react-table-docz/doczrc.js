37bbb55e2a8198eb1ab4cd24a0e617fc69301ea8
Jason Clark <jason.clark@tcnbroadcasting.com>
2020-03-16 14:43:01 -0600

    Initial build, got things in a pretty decent state

---
+++ react-table-docz/doczrc.js
@@ -0,0 +1,20 @@
+export default {
+  title: 'React Table Documentation',
+  description: 'Documentation for the react-table library built in Docz',
+  menu: [
+    'Getting Started',
+    'Installation',
+    'Concepts',
+    'Quick Start',
+    'Examples',
+    'API',
+    'FAQ',
+    'Contributing',
+    'Code of Conduct',
+    'Changelog',
+    'TypeScript',
+  ],
+  themeConfig: {
+    initialColorMode: 'dark',
+  },
+}
